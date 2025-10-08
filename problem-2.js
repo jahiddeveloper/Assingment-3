cda2d022841735bc828d30376f9693e159614d91
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-10-08 09:57:32 +0600

    problem-2 solve

---
+++ problem-2.js
@@ -0,0 +1,16 @@
+/** Problem -02 ( Cycle or Laptop ) */
+
+var money = 10000;
+
+if(money >= 25000) {
+    console.log("Laptop")
+}
+else if(money >= 10000) {
+    console.log("Cycle")
+}
+else if (money < 9999 && money > 0) {
+    console.log("Chocolate");
+}
+else {
+    console.log("Correct your amount of money.")
+}
